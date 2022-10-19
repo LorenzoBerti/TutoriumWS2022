eb96a6c4f78f779a4c79a0667b253fe972e82546
Lorenzo <Lorenzo.Berti@campus.lmu.de>
2022-10-19 10:49:13 +0200

    try

--- src/com/lorenzoberti/session01/HelloWorld.java
+++ src/com/lorenzoberti/session01/HelloWorld.java
@@ -15,7 +15,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Hello World!");
+		System.out.println("Hello World!"); //some changes
+		// new changes
+		System.out.println("Hello again!"); // some changes
 
 	}
 
